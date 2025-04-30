3ce523c0f77220b1c88dd95f9c840bb7888859bd
Conor Manning <conor.manning@mondoshop.com>
2025-04-30 17:17:59 -0500

    yazi core

--- source/functions.sh
+++ source/functions.sh
@@ -8,6 +8,16 @@ function cpf() {
 	cat $1 | pbcopy
 }
 
+# Yazi nonsense
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
 # Check expiration of certificate
 function check-cert() {
   local DOMAIN=$1
